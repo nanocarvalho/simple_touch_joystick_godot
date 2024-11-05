ed67bf9372146da163c904cc9ae46d4107a57b1d
Nano Carvalho <nanoc.contato@gmail.com>
2024-11-04 21:59:10 -0300

    Reorganized in a Joystick folder to not lost the imports paths

--- Joystick/touch_joy_instance.gd
+++ Joystick/touch_joy_instance.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _enter_tree() -> void:
-	var joy_touch: TouchScreenButton = preload("res://joystick_touch.tscn").instantiate()
+	var joy_touch: TouchScreenButton = preload("res://Joystick/joystick_touch.tscn").instantiate()
 	remove_child($placement)
 	add_child(joy_touch)
 
