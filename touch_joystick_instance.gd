815d23d225fbb14e491fb34e97df7d32b20d7d69
Nano Carvalho <nanoc.contato@gmail.com>
2024-11-04 21:46:47 -0300

    ops I left a duplicate of the touch_joy_instance.gd haha

--- touch_joystick_instance.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _enter_tree() -> void:
-	var joy_touch: TouchScreenButton = preload("res://joystick_touch.tscn").instantiate()
-	remove_child($placement)
-	add_child(joy_touch)
-
-func _get_direction() -> Vector2:
-	return get_child(0).direction_vector
